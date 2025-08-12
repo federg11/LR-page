55808d8630793071fa637166cb714c97f5d5b667
Fede <federicorg07@gmail.com>
2025-08-12 13:16:24 -0300

    cambios en el componente card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,10 +4,10 @@ const Card = ({ imageUrl, subtitle }) => {
   return (
       <>
       <div 
-      className="relative w-full h-[28rem] bg-cover bg-center rounded-lg shadow-2xl overflow-hidden"
+      className="relative w-full h-64 sm:h-72 md:h-80 lg:h-96 bg-cover bg-center rounded-lg shadow-2xl overflow-hidden flex"
       style={{ backgroundImage: `url(${imageUrl})` }}
       >
-      <div className="absolute bottom-0 left-0 w-full p-4 bg-red-800 bg-opacity-50 text-white">
+      <div className="absolute bottom-0 left-0 w-full p-3 sm:p-4 bg-red-800 bg-opacity-60 text-white">
         <h3 className="text-lg font-bold text-center">{subtitle}</h3>
       </div>
     </div>
