6de9e21a72c6e6465287e12aa604ab6924be6b49
Fede <federicorg07@gmail.com>
2026-08-27 11:30:00 -0300

    cambio red social

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -40,6 +40,7 @@ const Footer = () => {
             </svg>
           </a>
           <a
+            target="_blank"
             className="w-8 h-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
             href={social.linkedin}
           >
