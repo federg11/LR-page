0575d80ed5a5edb4096ca4d61bfb4f5b97f94a8a
Fede <federicorg07@gmail.com>
2026-08-28 11:20:21 -0300

    cambios services section añadidos

--- src/components/sections/Services/Services.jsx
+++ src/components/sections/Services/Services.jsx
@@ -70,7 +70,7 @@ const Services = () => {
               className="cursor-pointer inline-block"
           >
             <img
-              className="w-44 sm:w-48 md:w-80 h-auto object-contain mix-blend-multiply"
+              className="w-48 sm:w-52 md:w-80 h-auto object-contain mix-blend-multiply"
               src="/LogoCompleto.jpg"
               alt="Logo SMS Latinoamérica"
             />
