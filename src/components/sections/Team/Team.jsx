c38b512d59cd5ee9d530f26e7bd49d994050e956
Fede <federicorg07@gmail.com>
2026-08-28 11:31:43 -0300

    cambios team section altura

--- src/components/sections/Team/Team.jsx
+++ src/components/sections/Team/Team.jsx
@@ -5,7 +5,7 @@ const Team = () => {
   return (
     <main
       id="nosotros"
-      className="min-h-[60vh] sm:min-h-[70vh] md:min-h-[80vh] lg:min-h-dvh bg-red-800 text-neutral-900 dark:text-neutral-100 p-4 sm:p-6 scroll-mt-20"
+      className="bg-red-800 text-neutral-900 dark:text-neutral-100 px-4 sm:px-6 py-16 sm:py-20 scroll-mt-20"
     >
       <div className="mx-auto max-w-7xl">
         <h2 className="text-2xl sm:text-3xl font-semibold mb-4 text-center py-10">
